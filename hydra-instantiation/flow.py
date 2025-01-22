c36fad7a47e72842d226e087379552badd6c44c2
Eddie Mattia <eddie@outerbounds.com>
2025-01-22 09:24:58 -0800

    add hydra instantiation

---
+++ hydra-instantiation/flow.py
@@ -0,0 +1,25 @@
+from metaflow import FlowSpec, step, Config, conda_base
+
+
+@conda_base(packages={"hydra-core": "1.3.2"})
+class MyFlow(FlowSpec):
+
+    config = Config(name="config", default_value="")
+
+    @step
+    def start(self):
+        from omegaconf import OmegaConf
+        from hydra.utils import instantiate
+        
+        cfg_dict = self.config._data
+        cfg = OmegaConf.create(cfg_dict) 
+        instance = instantiate(cfg)
+        print(f"Instantiated: {instance}")
+        self.next(self.end)
+
+    @step
+    def end(self):
+        pass
+
+if __name__ == '__main__':
+    MyFlow()
